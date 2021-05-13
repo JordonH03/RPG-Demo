18638c5800ac1d1be4f478c0cf077464dbfdd754
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2021-05-12 20:51:23 -0500

    Add movement animation
    Added animation player and tree for player movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,6 +7,11 @@ const FRICTION = 500
 # Initializes player velocity variable
 var velocity = Vector2.ZERO
 
+# Animation variables on ready
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	# Calculates the direciton of movement based on key input
@@ -15,8 +20,12 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized() # reduces the velocity to the smallest unit...1
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta) # moves player to 0,0 by FRICTION val
 		
 	# Commands player body to stop on collision
