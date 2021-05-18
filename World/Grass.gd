8ad5b7e7c41b361bb1649328ade8c9634de08a20
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2021-05-17 21:02:14 -0500

    Add roll animation and hitboxes
    Added roll animation and hitboxes to player and grass

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,11 +1,14 @@
 extends Node2D
-
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var GrassEffect = load("res://Effects/GrassEffect.tscn") # Save scene as variable
-		var grassEffect = GrassEffect.instance()                 # Create instance/copy of scene
-		var world = get_tree().current_scene                     # Access world in file tree
-		world.add_child(grassEffect)                             # Add scene instance to world
-		grassEffect.global_position = global_position            # Set scene position
-		queue_free()                                             # Remove grass
 		
+func create_grass_effect():
+	var GrassEffect = load("res://Effects/GrassEffect.tscn") # Save scene as variable
+	var grassEffect = GrassEffect.instance()                 # Create instance/copy of scene
+	var world = get_tree().current_scene                     # Access world in file tree
+	world.add_child(grassEffect)                             # Add scene instance to world
+	grassEffect.global_position = global_position            # Set scene position
+
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
+	
